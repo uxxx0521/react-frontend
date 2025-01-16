2ceb4d355bbe5338c64557b9e3ab1c045207ad0d
Chengzhe Liu <uxxx@Mac.lan>
2025-01-16 07:33:31 -0800

    Added auto log-in using access/refresh token

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -2,6 +2,8 @@ import Button_Expense_Tracker from "../Button/Button_Expense_tracker";
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+
 
 
 function Sign_in() {
@@ -32,6 +34,55 @@ function Sign_in() {
       setMessage("An error occurred. Please try again.");
     }
   };
+
+  // Check token for auto login.
+  const checkTokens = async () => {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+
+
+      // Check if access token exists and is valid
+      if (accessToken) {
+        const decodedToken = jwt_decode(accessToken);
+        if (decodedToken.exp > Date.now() / 1000) {
+          // Access token is valid, redirect to user main page
+          console.log("Access token is valid.");
+          navigate("/portfolio/expense_tracker/me");
+          return;
+        }
+      }
+      console.log("accesToken is expired or not exist.");
+
+      // If access token is expired, try to use refresh token
+      const response = await axios.post("http://localhost:3000/refresh", {}, { withCredentials: true });
+
+      if (response.status === 200) {
+        const newAccessToken = response.data.accessToken;
+        localStorage.setItem("accessToken", newAccessToken);
+        console.log("New access token obtained via refresh token.");
+        navigate("/portfolio/expense_tracker/me");
+      } else {
+        console.log("Unable to refresh token, redirecting to login.");
+        navigate("/portfolio/expense_tracker/sign_in");
+        setMessage("Please log in.");
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        console.error("Unauthorized: Refresh token is invalid or expired.");
+      } else if (error.response.status === 403) {
+        console.error("Forbidden: You do not have access to this resource.");
+      } else {
+        console.error("Error during token check:", error.message);
+      }
+      navigate("/portfolio/expense_tracker/sign_in");
+    }
+  };
+
+  // check token when log in page mounts.
+  useEffect(() => {
+    checkTokens();
+  }, []);
+
 
   return (
     <>
